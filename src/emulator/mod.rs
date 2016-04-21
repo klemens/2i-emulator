db7a6f4e663d5dbac7d583a85f60321d0a76ea56
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-04-22 00:13:17 +0200

    Implement Display for our Error type

--- src/emulator/mod.rs
+++ src/emulator/mod.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::result;
 
 pub mod alu;
@@ -9,6 +10,15 @@ pub mod instruction;
 pub enum Error {
     Bus(&'static str),
     Cpu(&'static str),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Error::Bus(s) => write!(f, "Bus error: {}", s),
+            &Error::Cpu(s) => write!(f, "Cpu error: {}", s),
+        }
+    }
 }
 
 pub type Result<T> = result::Result<T, Error>;
