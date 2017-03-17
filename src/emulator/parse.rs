79a83dcc802a9754f9999475b7bc024ca62bfd7e
Klemens Schölhorn <klemens@schoelhorn.eu>
2017-03-17 03:08:13 +0100

    Add parser function that only returns reachable instructions
    This is useful when displaying or exporting the program and is done using a
    simple graph-reachability algorithm (adding new reachable instructions as
    long as there are any new ones). This works fine in practice, because programs
    tend to jump to higher addresses more then to lower ones.

--- src/emulator/parse.rs
+++ src/emulator/parse.rs
@@ -26,6 +26,105 @@ use super::instruction::Instruction;
 /// 00001: 00,00000 01 000|0000 01 10,0000 0
 /// ```
 pub fn read_program<R: Read>(reader: R) -> Result<[Instruction; 32]> {
+    let instructions = parse_instructions(reader)?;
+
+    // Replace all None values with zero instructions and remove the Option
+    let mut final_instructions = [Instruction::default(); 32];
+    for (i, instruction) in instructions.iter().enumerate() {
+        if let &Some(instruction) = instruction {
+            final_instructions[i] = instruction;
+        }
+    }
+
+    Ok(final_instructions)
+}
+
+/// Iterator stored on the stack with variable length and storage size of 2
+macro_rules! alternative_2 {
+    // TODO: Using a custom iterator instead of once would be more efficient
+    ($first:expr) => (
+        ::std::iter::once($first).chain(::std::iter::once($first)).take(1)
+    );
+    ($first:expr, $second:expr) => (
+        ::std::iter::once($first).chain(::std::iter::once($second)).take(2)
+    );
+}
+
+/// Parse 2i programs in string representation and return only the reachable
+/// instructions.
+///
+/// Instructions are considered reachable if there is a chain of instructions
+/// starting from the first one at address 0 to it. This also considers
+/// conditional jumps.
+///
+/// For details on the syntax of the string representation see `read_program`.
+pub fn read_reachable_program<R: Read>(reader: R) -> Result<Vec<(u8, Instruction)>> {
+    #[derive(Clone, Copy)]
+    enum S {
+        Empty, // Not yet visited
+        Visited, // Visited, but instruction is missing (will get default one)
+        Instruction(Instruction), // Visited and containing a instruction
+    }
+
+    let instructions = parse_instructions(reader)?;
+    let mut reachable_instructions = [S::Empty; 32];
+
+    // The instruction at address 0 is reachable by definition if it exists
+    reachable_instructions[0] = if let Some(inst) = instructions[0] {
+        S::Instruction(inst)
+    } else {
+        return Err(Error::Parse("No instruction reachable"));
+    };
+
+    // Since instructions can jump to earlier addresses, we have to iterate
+    // until no new instruction is found.
+    let mut finished = false;
+    while !finished {
+        finished = true;
+
+        for i in 0..reachable_instructions.len() {
+            if let S::Instruction(inst) = reachable_instructions[i] {
+                let na = inst.get_next_instruction_address();
+
+                // Consider both target addresses for conditional jumps
+                let target_addresses = if inst.get_address_control() == 0 {
+                    alternative_2!(na)
+                } else {
+                    alternative_2!(na & !1u8, na | 1u8)
+                };
+
+                for addr in target_addresses {
+                    let addr = addr as usize;
+                    // Only update instruction addresses that were not yet
+                    // visited. This ensures that the algorithm terminates
+                    if let S::Empty = reachable_instructions[addr] {
+                        finished = false;
+                        if let Some(inst) = instructions[addr] {
+                            reachable_instructions[addr] = S::Instruction(inst);
+                        } else {
+                            reachable_instructions[addr] = S::Visited;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Addresses which were visited but did not have a valid instruction get
+    // a default one (NOP, JMP 0)
+    Ok(reachable_instructions.iter().enumerate().filter_map(|(i,inst)| {
+        match *inst {
+            S::Empty => None,
+            S::Visited => Some((i as u8, Instruction::default())),
+            S::Instruction(inst) => Some((i as u8, inst)),
+        }
+    }).collect())
+}
+
+/// Actually parse the instructions from the given reader
+///
+/// For details on the syntax of the string representation see `read_program`.
+fn parse_instructions<R: Read>(reader: R) -> Result<[Option<Instruction>; 32]> {
     let mut instructions = [None; 32];
 
     let reader = BufReader::new(reader);
@@ -71,15 +170,7 @@ pub fn read_program<R: Read>(reader: R) -> Result<[Instruction; 32]> {
         }
     }
 
-    // Replace all None values with zero instructions and remove the Option
-    let mut final_instructions = [Instruction::new(0).unwrap(); 32];
-    for (i, instruction) in instructions.iter().enumerate() {
-        if let &Some(instruction) = instruction {
-            final_instructions[i] = instruction;
-        }
-    }
-
-    Ok(final_instructions)
+    Ok(instructions)
 }
 
 /// Convert a binary string to a u32 ignoring any chars other than 0 and 1
@@ -100,4 +191,49 @@ fn convert_binary_string_to_int(s: &str) -> u32 {
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn reachable_backjump() {
+        let program = Cursor::new("\
+            00000: 00 00100 000000000000000000\n\
+            00001: 00 11111 000000000000000000\n\
+            00010: 00 00001 000000000000000000\n\
+            00100: 00 00010 000000000000000000\n\
+            11111: 00 00000 000000000000000000\n\
+        ".to_owned());
+        assert_eq!(read_reachable_program(program).unwrap().as_slice(), &[
+            ( 0, Instruction::new(0b00_00100_000000000000000000).unwrap()),
+            ( 1, Instruction::new(0b00_11111_000000000000000000).unwrap()),
+            ( 2, Instruction::new(0b00_00001_000000000000000000).unwrap()),
+            ( 4, Instruction::new(0b00_00010_000000000000000000).unwrap()),
+            (31, Instruction::new(0b00_00000_000000000000000000).unwrap()),
+        ]);
+    }
+
+    #[test]
+    fn reachable_address_control() {
+        let program = Cursor::new("\
+            00000: 11 00010 000000000000000000\n\
+            00010: 00 00000 000000000000000000\n\
+            00011: 00 00000 000000000000000000\n\
+        ".to_owned());
+        assert_eq!(read_reachable_program(program).unwrap().as_slice(), &[
+            (0, Instruction::new(0b11_00010_000000000000000000).unwrap()),
+            (2, Instruction::new(0b00_00000_000000000000000000).unwrap()),
+            (3, Instruction::new(0b00_00000_000000000000000000).unwrap()),
+        ]);
+    }
+
+    #[test]
+    #[should_panic(expected = "No instruction reachable")]
+    fn reachable_empty() {
+        let program = Cursor::new("".to_owned());
+        read_reachable_program(program).unwrap();
+    }
 }
